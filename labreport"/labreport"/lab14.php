3a1a27a17daf78f0811b93e74d3715e93f99bb69
NirajShresthaNewar <136328579+NirajShresthaNewar@users.noreply.github.com>
2023-09-25 00:27:52 +0545

    Create lab14.php

---
+++ labreport"/labreport"/lab14.php
@@ -0,0 +1,22 @@
+<?php
+echo "start <br>";
+
+$servername = "localhost:80";
+$username = "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password);
+
+$sql = "CREATE DATABASE namuna_college";
+query($sql);
+
+
+if (!$conn){
+    die("Connecton error ". mysqli_connect_error());
+}
+else{
+    echo "Connection was successful";
+}
+
+?>
